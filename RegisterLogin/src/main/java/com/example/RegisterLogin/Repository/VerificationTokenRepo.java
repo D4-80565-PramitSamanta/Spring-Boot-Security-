74ab7453f6fc8d5706754b97918818ec7ea1add5
D4-80565-PramitSamanta <63332503+D4-80565-PramitSamanta@users.noreply.github.com>
2024-02-13 16:07:21 +0530

    had to delete all cos of version issues

--- RegisterLogin/src/main/java/com/example/RegisterLogin/Repository/VerificationTokenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.RegisterLogin.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.RegisterLogin.Entity.VerificationToken;
-
-public interface VerificationTokenRepo extends JpaRepository<VerificationToken, Long>{
-	
-}
